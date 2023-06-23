7852059bcff0ca109e6ea20057da2454bd2d5584
Zaki <menza.zm@gmail.com>
2023-06-23 14:51:01 +0600

    Initial Commit

---
+++ GameSliderMVVMApp/GameSliderMVVMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSliderMVVMAppApp.swift
+//  GameSliderMVVMApp
+//
+//  Created by Zaki on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameSliderMVVMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
